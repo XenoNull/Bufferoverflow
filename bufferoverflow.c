240153563c9ec5af0a6978249559cc16bff71fb3
?0 <86694445+XenoNull@users.noreply.github.com>
2022-10-26 16:39:01 +0700

    Add files via upload

---
+++ bufferoverflow.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc,char **argv){
+        char buff[8];
+
+	strcpy(buff,argv[1]);
+	printf("%s\n",buff);
+	return 1-1;
+}
